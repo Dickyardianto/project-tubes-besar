edff811daafa8ad65a2138dfd4bf8410a5b3ce86
dicky Ardianto <dikiardianto19@gmail.com>
2019-11-21 11:31:54 +0700

    Menambahkan tampilan content dengan static

--- application/views/home/viewHome.php
+++ application/views/home/viewHome.php
@@ -31,8 +31,58 @@
 <!-- Akhir Navbar -->
 <div class="jumbotron">
     <div class="container">
-        <h1 class="display-3  font-weight-bold">YUK BELI SAYUR</h1>
-        <h1 class="display-3  font-weight-bold">LANGSUNG KE PETANI !</h1>
+        <h1 class="display-3 text-light font-weight-bold">YUK BELI SAYUR</h1>
+        <h1 class="display-3 text-light font-weight-bold">LANGSUNG KE PETANI !</h1>
     </div>
 </div>
 <!-- Backgorund -->
+
+
+<!-- Content -->
+<div class="container">
+    <div class="row">
+        <div class="col-sm">
+            <div class="card mb-3">
+                <img src="<?= base_url() ?>assets/img/bgSayur.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <p class="card-text">Kotak Sortasi Angka / Kotak Pas / Number Shape Sorting / Mainan Anak Edukatif /
+                        Edukasi Aika Edu Toys</p>
+                    <p>Rp. 18.000</p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm">
+            <div class="card mb-3">
+                <img src="<?= base_url() ?>assets/img/bgSayur1.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <p class="card-text">Kotak Sortasi Angka / Kotak Pas / Number Shape Sorting / Mainan Anak Edukatif /
+                        Edukasi Aika Edu Toys</p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm">
+            <div class="card mb-3">
+                <img src="<?= base_url() ?>assets/img/bgSayur.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <p class="card-text">Kotak Sortasi Angka / Kotak Pas / Number Shape Sorting / Mainan Anak Edukatif /
+                        Edukasi Aika Edu Toys</p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm">
+            <div class="card mb-3">
+                <img src="<?= base_url() ?>assets/img/bgSayur.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <p class="card-text">Kotak Sortasi Angka / Kotak Pas / Number Shape Sorting / Mainan Anak Edukatif /
+                        Edukasi Aika Edu Toys</p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                </div>
+            </div>
+        </div>
+
+
+    </div>
+    <!-- Akhir Content -->
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="<?= base_url() ?>assets/css/style.css">
+    <link rel="stylesheet" href="<?= base_url(); ?>assets/css/style.css">
     <link rel="stylesheet" href="<?= base_url(); ?>assets/css/bootstrap.min2.css">
 
     <title><?= $title; ?></title>
