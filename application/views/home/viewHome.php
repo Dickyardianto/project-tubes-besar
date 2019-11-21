d76bfc4fde4c1a31d954a886c5200ed8de6d5fba
dicky Ardianto <dikiardianto19@gmail.com>
2019-11-21 10:37:10 +0700

    Menambahkan gambar background jumbotron pada tampilan awal

--- application/views/home/viewHome.php
+++ application/views/home/viewHome.php
@@ -1,15 +1,38 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<!-- Navbar -->
+<nav class="navbar navbar-expand-lg navbar-light bg-dark fixed-top">
     <div class="container">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
-            aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <a class="navbar-brand text-light" href="#">SAYURKEUN</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
+            aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav">
-                <a class="nav-item nav-link active" href="#">Toko</a>
-                <a class="nav-item nav-link active" href="#">Login</a>
-            </div>
+        <div id="navbarNavDropdown" class="navbar-collapse collapse">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link text-light" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-light" href="#">Features</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-light" href="#">Pricing</a>
+                </li>
+
+            </ul>
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link text-light" href="">Login</a>
+                </li>
+            </ul>
         </div>
     </div>
 </nav>
+
+<!-- Akhir Navbar -->
+<div class="jumbotron">
+    <div class="container">
+        <h1 class="display-3  font-weight-bold">YUK BELI SAYUR</h1>
+        <h1 class="display-3  font-weight-bold">LANGSUNG KE PETANI !</h1>
+    </div>
+</div>
+<!-- Backgorund -->
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -7,9 +7,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="<?= base_url() ?>assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?= base_url() ?>assets/css/style.css">
+    <link rel="stylesheet" href="<?= base_url(); ?>assets/css/bootstrap.min2.css">
 
-    <title><?= $title ?></title>
+    <title><?= $title; ?></title>
 </head>
 
 <body>
