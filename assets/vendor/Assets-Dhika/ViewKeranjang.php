1469a7d19122795f617fc3dcfdd3a5284b6d1afe
dhikafadill <dhikafadill@gmail.com>
2019-12-13 12:24:13 +0700

    add Halaman ViewKeranjang

---
+++ assets/vendor/Assets-Dhika/ViewKeranjang.php
@@ -0,0 +1,232 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="style/style.css">
+
+    <title>Niaga Sayur</title>
+    <link rel="shortcut icon" type="image/x-icon" href="images/logo/logonsbig.png" />
+</head>
+
+<body>
+
+
+    <!-- Navbar/Header -->
+    <div class="container-fluid">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+            <a class="navbar-brand" href="#">
+                <img src="images/logo/logonsbig.png" width="30" height="30" class="d-inline-block align-top" alt="">
+                Niaga Sayur</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <div class="navbar-nav mr-auto">
+                    <a class="nav-item nav-link active" href="#">Kategori</a>
+                    <!-- <form class="form-inline my-2 my-lg-0">
+                        <input class="form-control mr-sm-2 ml-3" type="search" placeholder="cari sayur disini !" aria-label="Search">
+                        </form> -->
+                </div>
+                <ul class="nav navbar-nav navbar-right">
+                    <li> <a class="navbar-brand mr-4" href="#">
+                            <img src="images/png/2.png" width="30" height="30"
+                                class="d-inline-block align-top my-2 my-sm-0" alt=""></a></li>
+                    <li><button class="btn btn-outline-light my-2 my-sm-0" type="submit">Login</button></li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+    <!-- Akhir Navbar/Header -->
+
+    <!-- Content Keranjang -->
+    <div class="container" style="margin-top:100px;">
+        <div class="row">
+
+            <div class="col-md-7">
+                <div class="card">
+                    <div class="card-body text-dark">
+                        <div class="p-2 pl-3 mb-3 ">
+                        <h4 class="font-weight-bold">Semua Produk.</h4>
+                        </div>
+                       
+                        <!-- DataPembelianBarang -->
+                        <div class="card mb-3 coba">
+                            <img src="....."
+                                class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <p class="card-text">....</p>
+                                <p style="color: #d71149;">Rp.10.000.000</p>
+                                <p class="card-text"><small class="text-muted">ditambahkan pada ...date..</small></p>
+                                </p>
+                            </div>
+                        </div>
+
+                        <div class="card mb-3 coba">
+                            <img src="....."
+                                class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <p class="card-text">Sayuran Lokal Wortel Indonesia Enak Banget</p>
+                                <p style="color: #d71149;">Rp.10.000.000</p>
+                                <p class="card-text"><small class="text-muted">ditambahkan pada ...date..</small></p>
+                                </p>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-5 bg-dark p-1">
+                <div class="card">
+                    <div class="card-body">
+                    <div class="p-2 pl-3 mb-3">
+                        <h4 class="font-weight-bold">Ringkasan Belanja.</h4>
+                        </div>
+                        <div class="col-md-6 d-inline-block p-2">
+                            <p>Sayuran Lokal Wortel Indonesia Enak Banget</p>
+                        </div>
+                        <div class="col-md-5 d-inline-block text-right" style="padding-left:20px">
+                            <p>Rp.40.000.00</p>
+                        </div>
+                        <div class="col-md-6 d-inline-block p-2">
+                            <p>Sayuran Lokal Wortel Indonesia Enak Banget</p>
+                        </div>
+                        <div class="col-md-5 d-inline-block text-right" style="padding-left:20px">
+                            <p>Rp.40.000.000</p>
+                        </div>
+                        <div class="col-md-6 d-inline-block p-2">
+                            <p>Sayuran Lokal Wortel Indonesia Enak Banget</p>
+                        </div>
+                        <div class="col-md-5 d-inline-block text-right" style="padding-left:20px">
+                            <p>Rp.40.000.000</p>
+                        </div>
+                        <div class="col-md-6 d-inline-block p-2">
+                            <p>Sayuran Lokal Wortel Indonesia Enak Banget</p>
+                        </div>
+                        <div class="col-md-5 d-inline-block text-right" style="padding-left:20px">
+                            <p>Rp.40.000.000</p>
+                        </div>
+                        <div class="col-md-6 d-inline-block p-2 mt-3">
+                            <p>Total Harga</p>
+                        </div>
+                        <div class="col-md-5 d-inline-block text-right " style="padding-left:20px">
+                            <p>Rp.160.000.000</p>
+                        </div>
+                        <div class="col-md-12 p-2">
+                        <form action="">
+                        <button type="button" class="btn btn-primary btn-lg btn-block">Beli</button>
+                        </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- Akhir Content Keranjang -->
+
+
+    <!-- Akhir Content -->
+
+    <!-- Footer -->
+    <div class="container-fluid bg-dark mt-5">
+
+        <div class="row">
+
+            <div class="col-sm-4">
+                <div class="card bg-dark text-light" style="width: 100%;">
+                    <div class="card-body">
+                        <h5 class="card-title">Kontak.</h5>
+                        <p class="card-text mb-0 small">Bandung Setiabudhi.</p>
+                        <p class="card-text mb-0 small">Call Center: 029317492746</p>
+                        <p class="card-text small">SMS/WA: 092371846283</p>
+                        <div class="card-text d-inline-block">
+                            <a href="#"><img src="images/medsos/ig.png" width="30" height="30" alt=""></a>
+                            <a href="#"><img src="images/medsos/fb.png" width="30" height="30" class="ml-3" alt=""></a>
+                            <a href="#"><img src="images/medsos/twt.png" width="30" height="30" class="ml-3" alt=""></a>
+                            <a href="#"><img src="images/medsos/yt.png" width="30" height="30" class="ml-3" alt=""></a>
+                            <a href="#"><img src="images/medsos/path.png" width="30" height="30" class="ml-3"
+                                    alt=""></a>
+                            <a href="#"><img src="images/medsos/line.png" width="30" height="30" class="ml-3"
+                                    alt=""></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-4">
+                <div class="card bg-dark text-light" style="width: 100%;">
+                    <div class="card-body">
+                        <h5 class="card-title">Informasi & Layanan.</h5>
+                        <p class="card-text small mb-0">Tentang Kami</p>
+                        <p class="card-text small mb-0">Syarat dan Ketentuan</p>
+                        <p class="card-text small mb-0">Alur Niaga Sayuran</p>
+                        <p class="card-text small mb-0">Partner</p>
+                        <p class="card-text small">FAQ</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-4">
+                <div class="card bg-dark text-light" style="width: 100%;">
+                    <div class="card-body">
+                        <h5 class="card-title">Tentang Niaga Sayur.</h5>
+                        <p class="card-text small">Niaga Sayur dibuat untuk memenuhi tugas besar rekweb. Lorem ipsum
+                            dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                            dolore magna aliqua. Ut enim ad minim veniam
+                            Laoreet sit amet cursus sit. Viverra justo nec ultrices dui sapien eget mi proin. Urna
+                            cursus eget nunc scelerisque viverra mauris.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="container my-auto">
+                <div class="copyright text-center my-auto text-light small">
+                    <span>Copyright &copy; Rekayasa Web Team | Universitas Pasundan <?= date('Y'); ?></span>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+    <!-- Akhir Footer -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+    </script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
