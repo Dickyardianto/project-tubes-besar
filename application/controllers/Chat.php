b22ec0f90b3afbb2d3eca4c4d4655a09bf407fb1
resthiana12 <resthiana123@gmail.com>
2019-12-26 09:38:38 +0700

    fix role id di fungsi chat

--- application/controllers/Chat.php
+++ application/controllers/Chat.php
@@ -23,7 +23,8 @@ class Chat extends CI_Controller
 
     public function index()
     {
-        if((isset($this->user->role_id)) != 2){
+        
+        if($this->session->userdata('role_id') != 2){
            $this->indexPetani($this->user->id);
         } else{
             $this->indexPembeli($this->user->id);
