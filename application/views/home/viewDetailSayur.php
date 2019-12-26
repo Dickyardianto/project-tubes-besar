f0289a6c4df50bb2ce81052297ad70cca481ab07
resthiana12 <resthiana123@gmail.com>
2019-12-26 17:33:14 +0700

    memperbaiki fitur chat

--- application/views/home/viewDetailSayur.php
+++ application/views/home/viewDetailSayur.php
@@ -189,14 +189,13 @@ jQuery(document).ready(function($) {
         false, // 9
         false // 10
     ];
-
     // New chat
     $(document).on('click', 'a[data-friend]', function(e) {
         var $data = $(this).data();
         if ($data.friend !== undefined && chatPosition.indexOf($data.friend) < 0) {
             var posRight = 0;
             var position;
-            for (var i in chatPosition) {
+            for(var i in chatPosition) {
                 if (chatPosition[i] == false) {
                     posRight = (i * 270) + 20;
                     chatPosition[i] = $data.friend;
@@ -214,7 +213,6 @@ jQuery(document).ready(function($) {
             initializeChat();
         }
     });
-
     // Minimize Maximize
     $(document).on('click', '.msg-wgt-header > a.name', function() {
         var parent = $(this).parent().parent();
@@ -224,7 +222,6 @@ jQuery(document).ready(function($) {
             parent.addClass('minimize');
         }
     });
-
     // Close
     $(document).on('click', '.msg-wgt-header > a.close', function() {
         var parent = $(this).parent().parent();
@@ -235,83 +232,51 @@ jQuery(document).ready(function($) {
             initializeChat();
         }, 1000)
     });
-
     var chatInterval = [];
-
     var initializeChat = function() {
         $.each(chatInterval, function(index, val) {
-            clearInterval(chatInterval[index]);
+            clearInterval(chatInterval[index]);   
         });
-
         $('.msg-wgt-active').each(function(index, el) {
             var $data = $(this).data();
             var $that = $(this);
             var $container = $that.find('.msg-wgt-message-container');
-
             chatInterval.push(setInterval(function() {
-
                 var oldscrollHeight = $container[0].scrollHeight;
                 var oldLength = 0;
-                $.post('<?= site_url('
-                    chat / getChats ') ?>', {
-                        chatWith: $data.chatWith
-                    },
-                    function(data, textStatus, xhr) {
-                        $that.find('a.name').text(data.name);
-                        // from last
-                        var chatLength = data.chats.length;
-                        var newIndex = data.chats.length;
-                        $.each(data.chats, function(index, el) {
-                            newIndex--;
-                            var val = data.chats[newIndex];
-
-                            var tpl = $('#msg-template').html();
-                            var tplBody = $('<div/>').append(tpl);
-                            var id = (val.chat_id + '_' + val.send_by +
-                                '_' + val.send_to).toString();
-
-
-                            if ($that.find('#' + id).length == 0) {
-                                tplBody.find('tbody').attr('id',
-                                    id); // set class
-                                tplBody.find('td.name').text(val
-                                    .nama); // set name
-                                tplBody.find('td.time').text(val
-                                    .time); // set time
-                                tplBody.find(
-                                        '.msg-wgt-message-list-body > td')
-                                    .html(nl2br(val
-                                        .message)); // set message
-                                $that.find('.msg-wgt-message-list').append(
-                                    tplBody.html()); // append message
-
-                                //Auto-scroll
-                                var newscrollHeight = $container[0]
-                                    .scrollHeight -
-                                    20; //Scroll height after the request
-                                if (newIndex === 0) {
-                                    $container.animate({
-                                            scrollTop: newscrollHeight
-                                        },
-                                        'normal'
-                                    ); //Autoscroll to bottom of div
-                                }
+                $.post('<?= site_url('chat/getChats') ?>', {chatWith: $data.chatWith}, function(data, textStatus, xhr) {
+                    $that.find('a.name').text(data.name);
+                    // from last
+                    var chatLength = data.chats.length;
+                    var newIndex = data.chats.length;
+                    $.each(data.chats, function(index, el) {
+                        newIndex--;
+                        var val = data.chats[newIndex];
+                        var tpl = $('#msg-template').html();
+                        var tplBody = $('<div/>').append(tpl);
+                        var id = (val.chat_id +'_'+ val.send_by +'_'+ val.send_to).toString();
+                        
+                        if ($that.find('#'+ id).length == 0) {
+                            tplBody.find('tbody').attr('id', id); // set class
+                            tplBody.find('td.name').text(val.nama); // set name
+                            tplBody.find('td.time').text(val.time); // set time
+                            tplBody.find('.msg-wgt-message-list-body > td').html(nl2br(val.message)); // set message
+                            $that.find('.msg-wgt-message-list').append(tplBody.html()); // append message
+                            //Auto-scroll
+                            var newscrollHeight = $container[0].scrollHeight - 20; //Scroll height after the request
+                            if (newIndex === 0) {
+                                $container.animate({ scrollTop: newscrollHeight }, 'normal'); //Autoscroll to bottom of div
                             }
-                        });
+                        }
                     });
+                });
             }, 1000));
-
             $that.find('textarea').on('keydown', function(e) {
                 var $textArea = $(this);
                 if (e.keyCode === 13 && e.shiftKey === false) {
-                    $.post('<?= site_url('
-                        chat / sendMessage ') ?>', {
-                            message: $textArea.val(),
-                            chatWith: $data.chatWith
-                        },
-                        function(data, textStatus, xhr) {});
+                    $.post('<?= site_url('chat/sendMessage') ?>', {message: $textArea.val(), chatWith: $data.chatWith}, function(data, textStatus, xhr) {
+                    });
                     $textArea.val(''); // clear input
-
                     e.preventDefault(); // stop 
                     return false;
                 }
@@ -319,12 +284,9 @@ jQuery(document).ready(function($) {
         });
     }
     var nl2br = function(str, is_xhtml) {
-        var breakTag = (is_xhtml || typeof is_xhtml === 'undefined') ? '<br ' + '/>' :
-            '<br>'; // Adjust comment to avoid issue on phpjs.org display
+        var breakTag = (is_xhtml || typeof is_xhtml === 'undefined') ? '<br ' + '/>' : '<br>'; // Adjust comment to avoid issue on phpjs.org display
         return (str + '').replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1' + breakTag + '$2');
     }
-
-
     // on load
     initializeChat();
 });
