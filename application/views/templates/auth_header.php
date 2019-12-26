22e628094cbd70d554f246ca71ebeee3123c2dea
dicky Ardianto <dikiardianto19@gmail.com>
2019-12-26 13:11:31 +0700

    Menambahkan icon pada view login

--- application/views/templates/auth_header.php
+++ application/views/templates/auth_header.php
@@ -20,6 +20,8 @@
     <!-- Custom styles for this template-->
     <link href="<?= base_url() ?>assets/css/sb-admin-2.min.css" rel="stylesheet">
     <link rel="stylesheet" href="<?= base_url() ?>assets/css/style.css">
+    <link rel="shortcut icon" type="image/x-icon"
+        href="<?= base_url(); ?>assets/vendor/Assets-Dhika/images/logo/logonsbig.png" />
 
 </head>
 
